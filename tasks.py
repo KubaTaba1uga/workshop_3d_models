2891eb30a7a6a24a46eafc069b2ec5d3940efe76
KubaTaba1uga <taba1uga6200@proton.me>
2024-05-27 17:38:02 +0000

    change proj layout

---
+++ tasks.py
@@ -0,0 +1,143 @@
+###############################################
+#                Imports                      #
+###############################################
+import os
+import subprocess
+
+from invoke import task
+
+###############################################
+#                Public API                   #
+###############################################
+ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+SRC_PATH = os.path.join(ROOT_PATH, "src")
+BUILD_PATH = os.path.join(ROOT_PATH, "build")
+CC = "openscad"
+
+
+@task
+def clean(c, bytecode=False, extra=""):
+    """
+    Clean up build and temporary files.
+
+    This task removes the specified patterns of files and directories,
+    including build artifacts, temporary files, and optionally Python
+    bytecode files.
+
+    Args:
+        c (invoke.context.Context): The context instance (automatically passed in by invoke).
+        bytecode (bool, optional): If True, also removes Python bytecode files (.pyc). Defaults to False.
+        extra (str, optional): Additional pattern to remove. Defaults to "".
+
+    Usage:
+        inv clean
+        inv clean --bytecode
+        inv clean --extra='**/*.log'
+    """
+    patterns = ["build", "**/*~", "**/#*", "*~", "#*", "**/*.stl"]
+
+    if bytecode:
+        patterns.append("**/*.pyc")
+    if extra:
+        patterns.append(extra)
+
+    for pattern in patterns:
+        c.run("rm -rf {}".format(pattern))
+
+
+@task
+def build(c, files_to_build: list = []):
+    """
+    Build STL files from OpenSCAD source files.
+
+    This task finds all OpenSCAD files in the source directory,
+    and generates corresponding STL files in the build directory.
+    If a list of specific files is provided, only those files will be built.
+
+    Args:
+        c (invoke.context.Context): The context instance (automatically passed in by invoke).
+        files_to_build (list, optional): List of specific OpenSCAD files to build. Defaults to [].
+
+    Usage:
+        inv build
+        inv build --files-to-build=example1.scad,example2.scad
+    """
+    if not _command_exists(CC):
+        print(f"{CC} need to be installed!")
+        exit(-1)
+
+    scad_files = _list_scad_files_recursively(SRC_PATH)
+
+    os.makedirs(BUILD_PATH, exist_ok=True)
+
+    for file_path in scad_files:
+        if files_to_build and (
+            not any((file_to_build in file_path) for file_to_build in files_to_build)
+        ):
+            continue
+
+        command = "openscad -o {} {}".format(_generate_build_name(file_path), file_path)
+        print(command)
+        c.run(command)
+        print()
+
+
+###############################################
+#                Private API                   #
+###############################################
+_SCAD_EXTENSION = ".scad"
+
+
+def _list_scad_files_recursively(root_dir):
+    files = []
+
+    for dirpath, dirnames, filenames in os.walk(root_dir):
+        for filename in filenames:
+            if _SCAD_EXTENSION == _get_file_extension(filename):
+                files.append(os.path.join(dirpath, filename))
+
+    return files
+
+
+def _get_file_extension(file_path):
+    _, file_extension = os.path.splitext(file_path)
+    return file_extension
+
+
+def _command_exists(command):
+    try:
+        # Attempt to run the command with '--version' or any other flag that doesn't change system state
+        subprocess.run(
+            [command, "--version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+        return True
+    except FileNotFoundError:
+        return False
+    except subprocess.CalledProcessError:
+        # The command exists but returned an error
+        return True
+    except Exception:
+        # Catch any other exceptions
+        return False
+
+
+def _generate_build_name(scad_file_path):
+    cut_path = scad_file_path.replace(_cut_path_to_directory(scad_file_path, "src"), "")
+
+    return os.path.join(
+        BUILD_PATH, ("_".join(cut_path.split("/"))[1:]).replace(_SCAD_EXTENSION, ".stl")
+    )
+
+
+def _cut_path_to_directory(full_path, target_directory):
+    """
+    Cuts the path up to the specified target directory.
+
+    :param full_path: The full path to be cut.
+    :param target_directory: The directory up to which the path should be cut.
+    :return: The cut path if the target directory is found, otherwise raises ValueError.
+    """
+    parts = full_path.split(os.sep)
+
+    target_index = parts.index(target_directory)
+    return os.sep.join(parts[: target_index + 1])
